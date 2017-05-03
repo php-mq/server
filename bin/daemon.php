ddb291eb0186d7f2837681b967ff658f583530d2
Holger Woltersdorf <hw@hollo.me>
2017-05-04 00:24:41 +0200

    Updates test daemon with current implementation

--- bin/daemon.php
+++ bin/daemon.php
@@ -5,13 +5,57 @@
 
 namespace hollodotme\PHPMQ;
 
-use hollodotme\PHPMQ\Defaults\Configs\DefaultEndpointConfig;
+use hollodotme\PHPMQ\Endpoint\Constants\SocketDomain;
+use hollodotme\PHPMQ\Endpoint\Constants\SocketType;
 use hollodotme\PHPMQ\Endpoint\Endpoint;
+use hollodotme\PHPMQ\Endpoint\Interfaces\ConfiguresEndpoint;
+use hollodotme\PHPMQ\Endpoint\Interfaces\IdentifiesSocketAddress;
+use hollodotme\PHPMQ\Endpoint\Types\UnixDomainSocket;
+use hollodotme\PHPMQ\MessageDispatchers\MessageDispatcher;
+use hollodotme\PHPMQ\MessageHandlers\AcknowledgementHandler;
+use hollodotme\PHPMQ\MessageHandlers\ConsumeRequestHandler;
+use hollodotme\PHPMQ\MessageHandlers\MessageC2EHandler;
+use hollodotme\PHPMQ\Storage\Interfaces\ConfiguresMessageQueue;
+use hollodotme\PHPMQ\Storage\MessageQueueSQLite;
 use Psr\Log\AbstractLogger;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$config = new DefaultEndpointConfig();
+$endpointConfig = new class implements ConfiguresEndpoint
+{
+	public function getSocketDomain() : int
+	{
+		return SocketDomain::UNIX;
+	}
+
+	public function getSocketType() : int
+	{
+		return SocketType::STREAM;
+	}
+
+	public function getSocketProtocol() : int
+	{
+		return 0;
+	}
+
+	public function getBindToAddress() : IdentifiesSocketAddress
+	{
+		return new UnixDomainSocket( '/tmp/phpmq.sock' );
+	}
+
+	public function getListenBacklog() : int
+	{
+		return SOMAXCONN;
+	}
+};
+
+$storageConfig = new class implements ConfiguresMessageQueue
+{
+	public function getMessageQueuePath() : string
+	{
+		return ':memory:';
+	}
+};
 
 $logger = new class extends AbstractLogger
 {
@@ -21,7 +65,16 @@ $logger = new class extends AbstractLogger
 	}
 };
 
-$endoint = new Endpoint( $config );
+$storage    = new MessageQueueSQLite( $storageConfig );
+$dispatcher = new MessageDispatcher( $storage );
+
+$endoint = new Endpoint( $endpointConfig, $dispatcher );
 $endoint->setLogger( $logger );
+
+$endoint->addMessageHandlers(
+	new MessageC2EHandler( $storage ),
+	new ConsumeRequestHandler(),
+	new AcknowledgementHandler( $storage )
+);
 
 $endoint->startListening();
