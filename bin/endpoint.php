62951f3bf9836a9027813f784eb584fd32247990
Holger Woltersdorf <hw@hollo.me>
2017-06-29 15:39:22 +0200

    Adds validation, configuration and error handling

--- bin/endpoint.php
+++ bin/endpoint.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php declare(strict_types=1);
 /**
  * @author hollodotme
@@ -5,80 +6,83 @@
 
 namespace PHPMQ\Server;
 
-use PHPMQ\Server\Configs\LogFileLoggerConfig;
-use PHPMQ\Server\Constants\AnsiColors;
+use PHPMQ\Server\Configs\ConfigBuilder;
 use PHPMQ\Server\Endpoint\Endpoint;
 use PHPMQ\Server\EventHandlers\Maintenance;
 use PHPMQ\Server\EventHandlers\MessageQueue;
 use PHPMQ\Server\Loggers\CompositeLogger;
-use PHPMQ\Server\Loggers\Constants\LogLevel;
-use PHPMQ\Server\Loggers\LogFileLogger;
 use PHPMQ\Server\Monitoring\ServerMonitor;
 use PHPMQ\Server\Monitoring\ServerMonitoringInfo;
 use PHPMQ\Server\Servers\MaintenanceServer;
 use PHPMQ\Server\Servers\MessageQueueServer;
 use PHPMQ\Server\Servers\ServerSocket;
-use PHPMQ\Server\Servers\Types\NetworkSocket;
-use PHPMQ\Server\Storage\Interfaces\ConfiguresSQLiteStorage;
-use PHPMQ\Server\Storage\SQLiteStorage;
-use Psr\Log\AbstractLogger;
+use PHPMQ\Server\Storage\Storage;
+use PHPMQ\Server\Validators\ArgumentValidator;
+use PHPMQ\Server\Validators\CompositeValidator;
+use PHPMQ\Server\Validators\PHPVersionValidator;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$storageConfigSQLite = new class implements ConfiguresSQLiteStorage
+$minPhpVersion     = '7.1.0';
+$packageVersion    = 'v0.1.0-dev';
+$defaultConfigFile = dirname( __DIR__ ) . '/config/phpmq.default.xml';
+
+$cliWriter = new CliWriter();
+$validator = new CompositeValidator();
+
+$validator->addValidators(
+	new PHPVersionValidator( $minPhpVersion, PHP_VERSION, PHP_BINARY, $packageVersion ),
+	new ArgumentValidator( $argv )
+);
+
+if ( $validator->failed() )
 {
-	public function getStoragePath() : string
+	foreach ( $validator->getMessages() as $message )
 	{
-//		return '/tmp/phpmq.sqlite';
-		return ':memory:';
+		fwrite( STDERR, $cliWriter->writeLn( $message )->getOutput() );
 	}
-};
 
-$outputLogger = new class extends AbstractLogger
+	exit( 1 );
+}
+
+try
 {
-	public function log( $level, $message, array $context = [] )
-	{
-		printf(
-			"[%s]: %s\n",
-			$level,
-			str_replace( array_keys( AnsiColors::COLORS ), AnsiColors::COLORS, $message )
-		);
-	}
-};
+	$configBuilder            = new ConfigBuilder( $argv[1] ?? $defaultConfigFile );
+	$logger                   = CompositeLogger::fromConfigBuilder( $configBuilder );
+	$storage                  = Storage::fromConfigBuilder( $configBuilder );
+	$serverMonitoringInfo     = ServerMonitoringInfo::fromStorage( $storage );
+	$messageQueueServerSocket = new ServerSocket( $configBuilder->getMessageQueueServerSocketAddress() );
+	$maintenanceServerSocket  = new ServerSocket( $configBuilder->getMaintenanceServerSocketAddress() );
+	$eventBus                 = new EventBus( $logger );
+	$serverMonitor            = new ServerMonitor( $serverMonitoringInfo, $cliWriter );
+	$endoint                  = new Endpoint( $eventBus, $serverMonitor, $logger );
 
-$logFileLogger = new LogFileLogger(
-	new LogFileLoggerConfig(
-		dirname( __DIR__ ) . '/build/logs/phpmq.log',
-		LogLevel::LOG_LEVEL_DEBUG
-	)
-);
+	$eventBus->addEventHandlers(
+		new MessageQueue\ClientConnectionEventHandler( $storage, $serverMonitoringInfo ),
+		new MessageQueue\ClientInboundEventHandler( $storage, $serverMonitoringInfo ),
+		new MessageQueue\ClientOutboundEventHandler( $storage, $serverMonitoringInfo ),
+		new Maintenance\ClientConnectionEventHandler( $cliWriter ),
+		new Maintenance\ClientInboundEventHandler( $storage, $cliWriter, $serverMonitoringInfo )
+	);
 
-$logger = new CompositeLogger();
-$logger->addLoggers( $outputLogger, $logFileLogger );
+	$endoint->registerServers(
+		new MessageQueueServer( $messageQueueServerSocket ),
+		new MaintenanceServer( $maintenanceServerSocket )
+	);
 
-$eventBus = new EventBus( $logger );
-$storage  = new SQLiteStorage( $storageConfigSQLite );
+	$endoint->run();
 
-$cliWriter            = new CliWriter();
-$serverMonitoringInfo = ServerMonitoringInfo::fromStorage( $storage );
+	exit( 0 );
+}
+catch ( \Throwable $e )
+{
+	$cliWriter->clearScreen( 'FAILURE' )
+	          ->writeLn( '<fg:red>ERROR:<:fg> ' . $e->getMessage() )
+	          ->writeLn( 'Exception: ' . get_class( $e ) )
+	          ->writeLn( 'In file %s on line %s', $e->getFile(), (string)$e->getLine() )
+	          ->writeLn( $e->getTraceAsString() );
 
-$eventBus->addEventHandlers(
-	new MessageQueue\ClientConnectionEventHandler( $storage, $serverMonitoringInfo ),
-	new MessageQueue\ClientInboundEventHandler( $storage, $serverMonitoringInfo ),
-	new MessageQueue\ClientOutboundEventHandler( $storage, $serverMonitoringInfo ),
-	new Maintenance\ClientConnectionEventHandler( $cliWriter ),
-	new Maintenance\ClientInboundEventHandler( $storage, $cliWriter, $serverMonitoringInfo )
-);
+	fwrite( STDERR, $cliWriter->getOutput() );
 
-$messageQueueServerSocket = new ServerSocket( new NetworkSocket( '127.0.0.1', 9100 ) );
-$maintenanceServerSocket  = new ServerSocket( new NetworkSocket( '127.0.0.1', 9101 ) );
-
-$serverMonitor = new ServerMonitor( $serverMonitoringInfo, $cliWriter );
-
-$endoint = new Endpoint( $eventBus, $serverMonitor, $logger );
-$endoint->registerServers(
-	new MessageQueueServer( $messageQueueServerSocket ),
-	new MaintenanceServer( $maintenanceServerSocket )
-);
-
-$endoint->run();
+	exit( 1 );
+}
