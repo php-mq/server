bbc689bc0e85476e58e08635a26f5d3f683c2350
Holger Woltersdorf <hw@hollo.me>
2017-07-02 00:34:27 +0200

    Fixes reading from and writing to socket in chunks

--- src/Clients/AbstractClient.php
+++ src/Clients/AbstractClient.php
@@ -28,53 +28,53 @@ abstract class AbstractClient implements CommunicatesWithServer
 		$this->socket   = $socket;
 	}
 
-	public function getClientId(): IdentifiesClient
+	public function getClientId() : IdentifiesClient
 	{
 		return $this->clientId;
 	}
 
-	public function read( int $bytes ): string
+	public function read( int $bytes ) : string
 	{
-		$buffer    = '';
-		$chunkSize = (int)min( $bytes, self::CHUNK_SIZE );
+		$buffer = '';
 
 		while ( $bytes > 0 )
 		{
-			$buffer    .= (string)fread( $this->socket, $chunkSize );
-			$bytes     -= self::CHUNK_SIZE;
 			$chunkSize = (int)min( $bytes, self::CHUNK_SIZE );
+			$buffer    .= (string)fread( $this->socket, $chunkSize );
+			$bytes    -= $chunkSize;
 		}
 
 		return $buffer;
 	}
 
-	public function collectSocket( array &$sockets ): void
+	public function collectSocket( array &$sockets ) : void
 	{
 		$sockets[ $this->clientId->toString() ] = $this->socket;
 	}
 
-	public function hasUnreadData(): bool
+	public function hasUnreadData() : bool
 	{
 		$metaData = stream_get_meta_data( $this->socket );
 
 		return ($metaData['unread_bytes'] > 0);
 	}
 
-	public function write( string $data ): int
+	public function write( string $data ) : int
 	{
 		for ( $written = 0, $writtenMax = strlen( $data ); $written < $writtenMax; $written += $bytes )
 		{
 			$bytes = @fwrite( $this->socket, substr( $data, $written ) );
-			if ( $bytes === false )
+
+			if ( !$bytes )
 			{
-				return $written;
+				return 0;
 			}
 		}
 
 		return $written;
 	}
 
-	public function shutDown(): void
+	public function shutDown() : void
 	{
 		stream_socket_shutdown( $this->socket, STREAM_SHUT_RDWR );
 		fclose( $this->socket );
