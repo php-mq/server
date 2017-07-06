8f2e911449d4d077e7b3d947c1385bde9dfaffde
Holger Woltersdorf <hw@hollo.me>
2017-07-06 22:54:36 +0200

    Adds mocking to event bus test

--- tests/Unit/EventBusTest.php
+++ tests/Unit/EventBusTest.php
@@ -6,34 +6,23 @@
 namespace PHPMQ\Server\Tests\Unit;
 
 use PHPMQ\Server\EventBus;
-use PHPMQ\Server\EventHandlers\AbstractEventHandler;
 use PHPMQ\Server\Tests\Unit\EventHandlers\Fixtures\TestEvent;
+use PHPMQ\Server\Tests\Unit\Fixtures\Traits\EventHandlerMocking;
 use PHPUnit\Framework\TestCase;
 use Psr\Log\NullLogger;
 
 final class EventBusTest extends TestCase
 {
-	public function testCanPublishEvents(): void
+	use EventHandlerMocking;
+
+	public function testCanPublishEvents() : void
 	{
 		$logger   = new NullLogger();
 		$eventBus = new EventBus( $logger );
 
-		$handler = new class extends AbstractEventHandler
-		{
-			protected function getAcceptedEvents(): array
-			{
-				return [ TestEvent::class ];
-			}
+		$eventBus->addEventHandlers( $this->getEventHandler() );
 
-			protected function whenTestEvent( TestEvent $event ): void
-			{
-				echo 'TestEvent was handled.';
-			}
-		};
-
-		$eventBus->addEventHandlers( $handler );
-
-		$this->expectOutputString( 'TestEvent was handled.' );
+		$this->expectOutputString( TestEvent::class . "\n" );
 
 		$eventBus->publishEvent( new TestEvent() );
 	}
