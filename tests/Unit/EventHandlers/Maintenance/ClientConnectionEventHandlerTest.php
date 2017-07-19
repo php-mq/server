c4d04bcbb0ea077d4fd40846c3c501b43c1e017f
Holger Woltersdorf <hw@hollo.me>
2017-07-19 22:24:57 +0200

    Adds maintenance client connection event handler tests

---
+++ tests/Unit/EventHandlers/Maintenance/ClientConnectionEventHandlerTest.php
@@ -0,0 +1,102 @@
+<?php declare(strict_types=1);
+/**
+ * @author hollodotme
+ */
+
+namespace PHPMQ\Server\Tests\Unit\EventHandlers\Maintenance;
+
+use PHPMQ\Server\CliWriter;
+use PHPMQ\Server\Endpoint\Interfaces\TransfersData;
+use PHPMQ\Server\EventHandlers\Maintenance\ClientConnectionEventHandler;
+use PHPMQ\Server\Events\Maintenance\ClientConnected;
+use PHPMQ\Server\Events\Maintenance\ClientDisconnected;
+use PHPMQ\Server\Loggers\AbstractLogger;
+use PHPMQ\Server\Streams\Stream;
+use PHPMQ\Server\Tests\Unit\Fixtures\Traits\SocketMocking;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\NullLogger;
+
+/**
+ * Class ClientConnectionEventHandlerTest
+ * @package PHPMQ\Server\Tests\Unit\EventHandlers\Maintenance
+ */
+final class ClientConnectionEventHandlerTest extends TestCase
+{
+	use SocketMocking;
+
+	/** @var TransfersData */
+	private $clientStream;
+
+	/** @var TransfersData */
+	private $remoteStream;
+
+	protected function setUp() : void
+	{
+		$this->setUpServerSocket();
+		$serverStream       = new Stream( $this->serverSocket );
+		$this->remoteStream = new Stream( $this->getRemoteClientSocket() );
+		$this->clientStream = $serverStream->acceptConnection();
+	}
+
+	protected function tearDown() : void
+	{
+		$this->remoteStream->close();
+		$this->clientStream->close();
+		$this->tearDownServerSocket();
+	}
+
+	/**
+	 * @param string $eventClass
+	 *
+	 * @dataProvider eventProvider
+	 */
+	public function testAcceptsEvents( string $eventClass ) : void
+	{
+		$handler = new ClientConnectionEventHandler( new CliWriter() );
+
+		$event = new $eventClass( $this->clientStream );
+
+		$this->assertTrue( $handler->acceptsEvent( $event ) );
+	}
+
+	public function eventProvider() : array
+	{
+		return [
+			[
+				'eventClass' => ClientConnected::class,
+			],
+			[
+				'eventClass' => ClientDisconnected::class,
+			],
+		];
+	}
+
+	public function testCanHandleClientConnectedEvent() : void
+	{
+		$event   = new ClientConnected( $this->clientStream );
+		$handler = new ClientConnectionEventHandler( new CliWriter() );
+		$handler->setLogger( new NullLogger() );
+
+		$handler->notify( $event );
+
+		$this->assertRegExp( '#phpmq#', $this->remoteStream->read( 1024 ) );
+	}
+
+	public function testCanHandleClientDisconnectedEvent() : void
+	{
+		$disconnectedEvent = new ClientDisconnected( $this->clientStream );
+		$handler           = new ClientConnectionEventHandler( new CliWriter() );
+		$logger            = new class extends AbstractLogger
+		{
+			public function log( $level, $message, array $context = [] )
+			{
+				echo $level . ' - ' . $message;
+			}
+		};
+		$handler->setLogger( $logger );
+
+		$handler->notify( $disconnectedEvent );
+
+		$this->expectOutputString( 'debug - Maintenance client disconnected: ' . $this->clientStream->getStreamId() );
+	}
+}
